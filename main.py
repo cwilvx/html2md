86e15f52e71ac042b477fca95921a38e75e4f2a0
mungai-njoroge <geoffreymungai45@gmail.com>
2023-10-23 20:08:35 +0300

    move function into a class & new context manager for opening files
    + update tests
    + use runTest method for single case  test cases
    + add test case to test the open file context manager

--- main.py
+++ main.py
@@ -1,154 +1,210 @@
+import logging
 import sys
+from typing import Any, Dict
 
 import requests
 from bs4 import BeautifulSoup
 from markdownify import MarkdownConverter
 
-domain = "https://wiki.debian.org"
-debian_news_url = domain + "/News"
-filepath = "debian_news.md"
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("H2D")
 
-title_id = "locationline"
-pageinfo_id = "pageinfo"
-content_id = "content"
+DOMAIN = "https://wiki.debian.org"
+DEBIAN_NEWS_URL = DOMAIN + "/News"
+FILEPATH = "debian_news.md"
+
+TITLE_ID = "locationline"
+PAGEINFO_ID = "pageinfo"
+CONTENT_ID = "content"
 
 
-def get_md(content, **options):
+class OpenFile:
     """
-    Converts a BeautifulSoup4 object to Markdown
-
-    :param content: BeautifulSoup object
-    :param options: Markdownify options
-    :return: Markdown string
+    A class that opens a file and returns a file object. Closes the file automatically
+    when the context is exited.
     """
-    markdown = MarkdownConverter(**options, heading_style="ATX").convert_soup(content)
-    markdown = markdown.strip() + "\n\n"
-    return markdown
 
+    def __init__(self, path: str, mode: str = "w", encoding: str = "utf-8") -> None:
+        """
+        Initializes the class with the file path, mode and encoding.
 
-def fetch_page(url: str):
-    """
-    Fetches the page content from the given URL
-    and returns it as a string. Exits if the request fails.
+        :param path: Path to the file
+        :param mode: File mode (default: "w")
+        :param encoding: File encoding (default: "utf-8")
+        """
+        self.path = path
+        self.mode = mode
+        self.encoding = encoding
 
-    :param url: URL of the page to fetch
-    :return: Page content as a string
-    """
-    response = requests.get(url)
+    def __enter__(self):
+        """
+        Opens the file and returns the file object.
 
-    if response.status_code != 200:
-        print(
-            f"Failed to retrieve the Debian News page. Status code: {response.status_code}"
-        )
-        sys.exit(1)
+        :return: File object
+        """
+        self.file = open(self.path, self.mode, encoding=self.encoding)
+        return self.file
 
-    return response.text
-
-
-def get_translations_frontmatter(page: BeautifulSoup):
-    """
-    Extracts the translation links from the page and returns them as a
-    frontmatter string.
-
-    :param page: BeautifulSoup object of the page
-    :return: Frontmatter entry string
-    """
-    translations_node = page.find("small")
-    translations = []
-
-    # for each a tag in the translations node, extract the href + text
-    for link in translations_node.find_all("a"):
-        href = link.get("href")
-        text = link.get_text()
-        translations.append(f"  - {text}: {href}")
-
-    # format the translations list as a frontmatter list entry
-    translations = "\n".join(translations)
-    return f"translations:\n{translations}\n"
-
-
-def get_frontmatter(page: BeautifulSoup):
-    """
-    Extracts metadata from the page and returns it as a
-    frontmatter string.
-
-    :param page: BeautifulSoup object of the page
-    :return: Frontmatter string
-    """
-    translations = get_translations_frontmatter(page)
-
-    page_title = page.find(id=title_id).get_text()
-    pageinfo = page.find(id=pageinfo_id).get_text()
-
-    # Extract last updated date from #pageinfo node
-    last_updated = pageinfo.split("modified")[1].replace(")", "").strip()
-
-    return f"---\ntitle: {page_title.strip()}\nlast_updated: {last_updated}\n{translations}---\n\n"
-
-
-def get_content(page: BeautifulSoup):
-    """
-    Returns the content of the page as a BeautifulSoup object
-
-    :param page: BeautifulSoup object of the page
-    :return: BeautifulSoup object of the content
-    """
-    content = page.find("div", {"id": content_id})
-
-    # remove translation links
-    content.find("small").decompose()  # first <small> node
-
-    # find all dt nodes and change them to h3
-    for dt in content.find_all("dt"):
-        dt.name = "h3"
-
-    # remove first horizontal line
-    content.find("hr").decompose()
-
-    return content
-
-
-def save_to_file(path: str, content: str):
-    """
-    Saves the content to the given path. Overwrites the file if it exists.
-
-    :param path: Path to the file
-    :param content: Content to write
-    """
-    with open(path, "w", encoding="utf-8") as file:
-        file.write(content)
-        print(f"Wrote {len(content)} bytes to {path}")
-
-
-def format_internal_links(page: BeautifulSoup):
-    """
-    Formats internal links to be relative to the current page.
-    """
-    for link in page.find_all("a"):
-        href = link.get("href")
-        if href.startswith("/"):
-            link["href"] = domain + href
-
-    return page
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Closes the file when the context is exited.
+        """
+        self.file.close()
 
 
 class PageToMd:
     """
-    Class that orchastrates the conversion of the Debian News page.
+    Converts a Debian News page to Markdown and saves it to a file.
     """
 
-    def __init__(self, url: str, path: str) -> None:
-        html = fetch_page(url)
-        page = BeautifulSoup(html, "html.parser")
-        page = format_internal_links(page)
+    def __init__(self, url: str = None, path: str = None) -> None:
+        """
+        Initializes the class and starts the conversion process.
 
-        frontmatter = get_frontmatter(page)
-        content = get_content(page)
-        markdown = get_md(content)
+        :param url: URL of the page to convert
+        :param path: Path to the file to write
+        """
+        self.url = url
+        self.path = path
+
+    def get_md(self, content: BeautifulSoup, **options: Dict[str, Any]) -> str:
+        """
+        Converts a BeautifulSoup4 object to Markdown
+
+        :param content: BeautifulSoup object
+        :param options: Markdownify options
+        :return: Markdown string
+        """
+        markdown = MarkdownConverter(**options, heading_style="ATX").convert_soup(
+            content
+        )
+        markdown = markdown.strip() + "\n\n"
+        return markdown
+
+    def fetch_page(self, url: str) -> str:
+        """
+        Fetches the page content from the given URL
+        and returns it as a string. Raises an exception if the request fails.
+
+        :param url: URL of the page to fetch
+        :return: Page content as a string
+        """
+
+        try:
+            response = requests.get(url)
+        except requests.exceptions.ConnectionError:
+            logger.error("Failed to connect to the internet!")
+            sys.exit(1)
+
+        if response.status_code != 200:
+            logger.error(
+                f"Failed to retrieve the Debian News page. Status code: {response.status_code}"
+            )
+            sys.exit(1)
+
+        return response.text
+
+    def get_translations_frontmatter(self, page: BeautifulSoup) -> str:
+        """
+        Extracts the translation links from the page and returns them as a
+        frontmatter string.
+
+        :param page: BeautifulSoup object of the page
+        :return: Frontmatter entry string
+        """
+        translations_node = page.find("small")
+        translations = []
+
+        # for each a tag in the translations node, extract the href + text
+        for link in translations_node.find_all("a"):
+            href = link.get("href")
+            text = link.get_text()
+            translations.append(f"  - {text}: {href}")
+
+        # format the translations list as a frontmatter list entry
+        translations = "\n".join(translations)
+        return f"translations:\n{translations}\n"
+
+    def get_frontmatter(self, page: BeautifulSoup) -> str:
+        """
+        Extracts metadata from the page and returns it as a
+        frontmatter string.
+
+        :param page: BeautifulSoup object of the page
+        :return: Frontmatter string
+        """
+        translations = self.get_translations_frontmatter(page)
+
+        page_title = page.find(id=TITLE_ID).get_text()
+        pageinfo = page.find(id=PAGEINFO_ID).get_text()
+
+        # Extract last updated date from #pageinfo node
+        last_updated = pageinfo.split("modified")[1].replace(")", "").strip()
+
+        return f"---\ntitle: {page_title.strip()}\nlast_updated: {last_updated}\n{translations}---\n\n"
+
+    def get_content(self, page: BeautifulSoup) -> BeautifulSoup:
+        """
+        Returns the content of the page as a BeautifulSoup object
+
+        :param page: BeautifulSoup object of the page
+        :return: BeautifulSoup object of the content
+        """
+        content = page.find("div", {"id": CONTENT_ID})
+
+        # remove translation links
+        content.find("small").decompose()  # first <small> node
+
+        # find all dt nodes and change them to h3
+        for dt in content.find_all("dt"):
+            dt.name = "h3"
+
+        # remove first horizontal line
+        content.find("hr").decompose()
+
+        return content
+
+    def save_to_file(self, path: str, content: str) -> None:
+        """
+        Saves the content to the given path. Overwrites the file if it exists.
+
+        :param path: Path to the file
+        :param content: Content to write
+        """
+        with OpenFile(path) as file:
+            file.write(content)
+            logger.info(f"Wrote {len(content)} bytes to {path}")
+
+    def format_internal_links(self, page: BeautifulSoup) -> BeautifulSoup:
+        """
+        Formats internal links to be relative to the current page.
+        """
+        for link in page.find_all("a"):
+            href = link.get("href")
+            if href.startswith("/"):
+                link["href"] = DOMAIN + href
+
+        return page
+
+    def run(self):
+        """
+        Runs the conversion process.
+        """
+        html = self.fetch_page(self.url)
+        page = BeautifulSoup(html, "html.parser")
+        page = self.format_internal_links(page)
+
+        frontmatter = self.get_frontmatter(page)
+        content = self.get_content(page)
+        markdown = self.get_md(content)
 
         text = frontmatter + markdown
-        save_to_file(path, text)
+        self.save_to_file(self.path, text)
 
 
 if __name__ == "__main__":
-    PageToMd(debian_news_url, filepath)
+    try:
+        converter = PageToMd(DEBIAN_NEWS_URL, FILEPATH)
+        converter.run()
+    except Exception as e:
+        logger.exception(e)
--- test_main.py
+++ test_main.py
@@ -1,20 +1,10 @@
 import os
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 from bs4 import BeautifulSoup
 
-from main import (
-    domain,
-    debian_news_url,
-    fetch_page,
-    get_content,
-    get_frontmatter,
-    get_md,
-    save_to_file,
-    format_internal_links,
-)
-
+from main import DEBIAN_NEWS_URL, DOMAIN, OpenFile, PageToMd
 
 last_updated = "2021-08-01"
 page_title = "News"
@@ -37,34 +27,34 @@ page_html = f"""
 
 class TestFetchPage(unittest.TestCase):
     @patch("main.requests.get")
-    def test_fetch_page_success(self, mock_get):
+    def test_success(self, mock_get):
         mock_response = MagicMock()
         mock_response.status_code = 200
         mock_response.text = "<html><body><h1>Hello, world!</h1></body></html>"
         mock_get.return_value = mock_response
 
-        result = fetch_page(debian_news_url)
+        result = PageToMd().fetch_page(DEBIAN_NEWS_URL)
 
         self.assertEqual(result, mock_response.text)
 
     @patch("main.requests.get")
-    def test_fetch_page_failure(self, mock_get):
+    def test_failure(self, mock_get):
         mock_response = MagicMock()
         mock_response.status_code = 404
         mock_get.return_value = mock_response
 
         with self.assertRaises(SystemExit):
-            fetch_page(debian_news_url + "/404")
+            PageToMd().fetch_page(DEBIAN_NEWS_URL + "/404")
 
 
 class TestGetFrontmatter(unittest.TestCase):
-    def test_get_frontmatter(self):
+    def runTest(self):
         page = BeautifulSoup(
             page_html,
             "html.parser",
         )
 
-        result = get_frontmatter(page)
+        result = PageToMd().get_frontmatter(page)
 
         self.assertEqual(
             result,
@@ -73,13 +63,13 @@ class TestGetFrontmatter(unittest.TestCase):
 
 
 class TestGetContent(unittest.TestCase):
-    def test_get_content(self):
+    def runTest(self):
         page = BeautifulSoup(
             page_html,
             "html.parser",
         )
 
-        result = get_content(page)
+        result = PageToMd().get_content(page)
         assert_to = page_content_html.replace(translations, "").replace("<hr/>", "", 1)
 
         self.assertEqual(
@@ -89,10 +79,10 @@ class TestGetContent(unittest.TestCase):
 
 
 class TestGetMd(unittest.TestCase):
-    def test_get_md(self):
+    def runTest(self):
         page = BeautifulSoup(page_html, "html.parser")
-        content = get_content(page)
-        result = get_md(content)
+        content = PageToMd().get_content(page)
+        result = PageToMd().get_md(content)
 
         self.assertEqual(result.strip(), "**Page content**\n\n---")
 
@@ -104,10 +94,10 @@ class TestSaveToFile(unittest.TestCase):
     def tearDown(self):
         os.remove(self.filepath)
 
-    def test_save_to_file(self):
+    def runTest(self):
         content = "Hello, world!"
 
-        save_to_file(self.filepath, content)
+        PageToMd().save_to_file(self.filepath, content)
 
         with open(self.filepath, "r") as f:
             result = f.read()
@@ -116,14 +106,30 @@ class TestSaveToFile(unittest.TestCase):
 
 
 class TestFormatInterlLinks(unittest.TestCase):
-    def test_format_internal_links(self):
+    def runTest(self):
         test_node_html = '<small><a href="/uk/News">Українська</a></small>'
         test_node = BeautifulSoup(
             test_node_html,
             "html.parser",
         )
 
-        result = str(format_internal_links(test_node)).strip()
-        should_be = test_node_html.replace("/uk/News", domain + "/uk/News")
+        result = str(PageToMd().format_internal_links(test_node)).strip()
+        should_be = test_node_html.replace("/uk/News", DOMAIN + "/uk/News")
 
         self.assertEqual(result, should_be)
+
+
+class TestOpenFile(unittest.TestCase):
+    def setUp(self):
+        self.filepath = "testfile.txt"
+
+    def tearDown(self):
+        os.remove(self.filepath)
+
+    def runTest(self):
+        content = "Hello, Debian!"
+
+        with OpenFile(self.filepath) as f:
+            f.write(content)
+
+        self.assertTrue(f.closed)
